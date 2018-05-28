ff889cce32dceada3abb30c20a57460550abc313
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-28 19:10:04 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  selected_arr = []
+  while i < collection.length
+    yield_result = yield(collection[i])
+    if yield_result == true 
+      selected_arr.push(collection[i])
+    end 
+    i+=1
+  end  
+  selected_arr
 end
