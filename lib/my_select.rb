167740931362813fa3eaa67896748837f0ba1671
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-28 19:09:49 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
   i = 0
   selected_arr = []
   while i < collection.length
-    yield(collection[i])
-    if yield(collection[i]) == true 
+    yield_result = yield(collection[i])
+    if yield_result == true 
       selected_arr.push(collection[i])
     end 
     i+=1
